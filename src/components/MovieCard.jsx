1534892f0b12f96d260336d66db45994ab2d57aa
ArunJeganathan1909 <jeganathanarun1105@gmail.com>
2025-05-10 20:02:43 +0530

    favorite added to movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { StarBorder, Star } from "@mui/icons-material"; // Import icons
 import "../styles/components/MovieCard.css";
 
 const MovieCard = ({ movie }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = (e) => {
+    e.preventDefault(); // Prevent navigating when clicking the star
+    setIsFavorite((prev) => !prev);
+  };
+
   return (
     <div className="movie-card">
+      {/* Star Icon */}
+      <div className="movie-card__star" onClick={toggleFavorite}>
+        {isFavorite ? <Star /> : <StarBorder />}
+      </div>
+
       <Link to={`/movie/${movie.id}`} className="movie-card__link">
         <img
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
